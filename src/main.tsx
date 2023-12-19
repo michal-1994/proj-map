1fb0870b4b893421b3425d8d95c7444e26b75ef2
Michal <michalgrzegorczyk04@gmail.com>
2023-12-19 13:49:55 +0100

    set high contrast for checkboxes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import './themes/contrast.css';
+import './themes/high-contrast.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
