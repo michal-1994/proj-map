506ca0b1e04f12e20d3d02eafb06835f9c34ccb7
Michal <michalgrzegorczyk04@gmail.com>
2024-01-02 12:22:01 +0100

    refactor: measurment - simplify methods

--- src/utils/tool-utils.tsx
+++ src/utils/tool-utils.tsx
@@ -21,29 +21,6 @@ export const toggleHighContrast = () => {
     document.getElementsByTagName('html')[0].classList.toggle('high-contrast');
 };
 
-export const formatLength = (line: any) => {
-    const length = getLength(line);
-    let output;
-    if (length > 100) {
-        output = Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
-    } else {
-        output = Math.round(length * 100) / 100 + ' ' + 'm';
-    }
-    return output;
-};
-
-export const formatArea = (polygon: any) => {
-    const area = getArea(polygon);
-    let output;
-    if (area > 10000) {
-        output =
-            Math.round((area / 1000000) * 100) / 100 + ' ' + 'km<sup>2</sup>';
-    } else {
-        output = Math.round(area * 100) / 100 + ' ' + 'm<sup>2</sup>';
-    }
-    return output;
-};
-
 export const switchMeasurmentTool = (map: Map, idOption: string) => {
     const source = new VectorSource();
     const vector = new VectorLayer({
@@ -52,32 +29,16 @@ export const switchMeasurmentTool = (map: Map, idOption: string) => {
     });
 
     let sketch: any;
-    let helpTooltipElement: any;
-    let helpTooltip: any;
     let measureTooltipElement: any;
     let measureTooltip: any;
     let draw: any;
-
-    const createHelpTooltip = () => {
-        if (helpTooltipElement) {
-            helpTooltipElement.parentNode.removeChild(helpTooltipElement);
-        }
-        helpTooltipElement = document.createElement('div');
-        helpTooltipElement.className = 'ol-tooltip hidden';
-        helpTooltip = new Overlay({
-            element: helpTooltipElement,
-            offset: [15, 0],
-            positioning: 'center-left'
-        });
-        map.addOverlay(helpTooltip);
-    };
 
     const createMeasureTooltip = () => {
         if (measureTooltipElement) {
             measureTooltipElement.parentNode.removeChild(measureTooltipElement);
         }
         measureTooltipElement = document.createElement('div');
-        measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
+        measureTooltipElement.style.color = 'black';
         measureTooltip = new Overlay({
             element: measureTooltipElement,
             offset: [0, -15],
@@ -88,58 +49,68 @@ export const switchMeasurmentTool = (map: Map, idOption: string) => {
         map.addOverlay(measureTooltip);
     };
 
-    const addInteraction = () => {
-        const type = idOption === POLYGON_MEASURMENT ? 'Polygon' : 'LineString';
-        draw = new Draw({
-            source: source,
-            type: type,
-            style: function (feature) {
-                const geometryType = feature?.getGeometry()?.getType();
-                if (geometryType === type || geometryType === 'Point') {
-                    return createMeasurmentPreviewStyle();
-                }
-            }
-        });
-        map.addInteraction(draw);
-
-        createMeasureTooltip();
-        createHelpTooltip();
-
-        let listener: EventsKey | EventsKey[];
-        draw.on('drawstart', function (evt: any) {
-            sketch = evt.feature;
-
-            let tooltipCoord = evt.coordinate;
-
-            listener = sketch.getGeometry().on('change', function (evt: any) {
-                const geom = evt.target;
-                let output;
-                if (idOption === POLYGON_MEASURMENT) {
-                    output = formatArea(geom);
-                    tooltipCoord = geom.getInteriorPoint().getCoordinates();
-                } else if (idOption === LINEAR_MEASURMENT) {
-                    output = formatLength(geom);
-                    tooltipCoord = geom.getLastCoordinate();
-                }
-                measureTooltipElement.innerHTML = output;
-                measureTooltip.setPosition(tooltipCoord);
-            });
-        });
-
-        draw.on('drawend', function () {
-            measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
-            measureTooltip.setOffset([0, -7]);
-            sketch = null;
-            measureTooltipElement = null;
-            createMeasureTooltip();
-            unByKey(listener);
-        });
-    };
-
     map?.addLayer(vector);
-
     map.removeInteraction(draw);
-    addInteraction();
+
+    const type = idOption === POLYGON_MEASURMENT ? 'Polygon' : 'LineString'; // TODO: refactor - add type to object Polygon | LineString
+    draw = new Draw({
+        source: source,
+        type: type,
+        style: function (feature) {
+            const geometryType = feature?.getGeometry()?.getType();
+            if (geometryType === type || geometryType === 'Point') {
+                return createMeasurmentPreviewStyle();
+            }
+        }
+    });
+    map.addInteraction(draw);
+
+    createMeasureTooltip();
+
+    let listener: EventsKey | EventsKey[];
+    draw.on('drawstart', function (evt: any) {
+        sketch = evt.feature;
+        let tooltipCoord = evt.coordinate;
+
+        listener = sketch.getGeometry().on('change', function (evt: any) {
+            const geom = evt.target;
+            let output;
+
+            if (idOption === POLYGON_MEASURMENT) {
+                const area = getArea(geom);
+                if (area > 10000) {
+                    output =
+                        Math.round((area / 1000000) * 100) / 100 +
+                        ' ' +
+                        'km<sup>2</sup>';
+                } else {
+                    output =
+                        Math.round(area * 100) / 100 + ' ' + 'm<sup>2</sup>';
+                }
+                tooltipCoord = geom.getInteriorPoint().getCoordinates();
+            } else if (idOption === LINEAR_MEASURMENT) {
+                const length = getLength(geom);
+                if (length > 100) {
+                    output =
+                        Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
+                } else {
+                    output = Math.round(length * 100) / 100 + ' ' + 'm';
+                }
+                tooltipCoord = geom.getLastCoordinate();
+            }
+            measureTooltipElement.innerHTML = output;
+            measureTooltip.setPosition(tooltipCoord);
+        });
+    });
+
+    draw.on('drawend', function () {
+        measureTooltipElement.style.color = 'black';
+        measureTooltip.setOffset([0, -7]);
+        sketch = null;
+        measureTooltipElement = null;
+        createMeasureTooltip();
+        unByKey(listener);
+    });
 };
 
 export const exportToPDF = (config: ExportModel) => {
