1d4a9b79ac4167421ebaa9e26c2b47ead982786c
Michal <michalgrzegorczyk04@gmail.com>
2024-01-08 10:21:49 +0100

    refactor: vite - split files by manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,24 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => {
     const config = {
         plugins: [react()],
-        base: '/'
+        base: '/',
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks(id: any) {
+                        if (id.includes('src/pages/HomePage.tsx')) {
+                            return 'homePage';
+                        } else if (
+                            id.includes('src/pages/ConfiguratorPage.tsx')
+                        ) {
+                            return 'configuratorPage';
+                        } else if (id.includes('src/pages/MapPage.tsx')) {
+                            return 'mapPage';
+                        }
+                    }
+                }
+            }
+        }
     };
 
     if (command !== 'serve') {
