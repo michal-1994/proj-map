d48482593ac3b59b0e5f369b70994202c44f7ada
Michal <michalgrzegorczyk04@gmail.com>
2024-01-09 11:47:22 +0100

    feat: map - add lazy loading and spinner

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { Suspense, lazy, useState } from 'react';
+import { GridLoader } from 'react-spinners';
 
 import MapNav from '../components/map/layout/MapNav';
 import MapSidebar from '../components/map/layout/MapSidebar';
-import MapView from '../components/map/layout/MapView';
 import MapPrint from '../components/map/tools/MapPrint';
 
 import { MapProvider } from '../context/map-context';
 import { ToolProvider } from '../context/tool-context';
+
+const MapView = lazy(() => import('../components/map/layout/MapView'));
 
 const MapPage = () => {
     const [isOpenSidebar, setIsOpenSidebar] = useState<boolean>(false);
@@ -15,16 +17,30 @@ const MapPage = () => {
         setIsOpenSidebar(!isOpenSidebar);
     };
 
+    const loadingElement = (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+            }}>
+            <GridLoader color="#0d6efd" />
+        </div>
+    );
+
     return (
         <MapProvider>
             <ToolProvider>
-                <MapNav />
-                <MapSidebar
-                    isOpen={isOpenSidebar}
-                    toggleSidebar={toggleSidebar}
-                />
-                <MapPrint />
-                <MapView />
+                <Suspense fallback={loadingElement}>
+                    <MapNav />
+                    <MapSidebar
+                        isOpen={isOpenSidebar}
+                        toggleSidebar={toggleSidebar}
+                    />
+                    <MapPrint />
+                    <MapView />
+                </Suspense>
             </ToolProvider>
         </MapProvider>
     );
