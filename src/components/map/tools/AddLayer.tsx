a678064d641ffff2dbd00311f7c134a061a8f156
Michal <michalgrzegorczyk04@gmail.com>
2024-01-15 09:44:26 +0100

    feat: addlayer - form validation

--- src/components/map/tools/AddLayer.tsx
+++ src/components/map/tools/AddLayer.tsx
@@ -1,33 +1,57 @@
 import React, { useState } from 'react';
-import { Modal, Form, Row, Col, Button } from 'react-bootstrap';
+import { Modal, Form, Row, Button } from 'react-bootstrap';
 
 import { useToolContext } from '../../../context/tool-context';
 
+import './AddLayer.css';
+
 const AddLayer: React.FC = () => {
-    const [layerName, setLayerName] = useState('');
-    const [linkToGeoJSON, setLinkToGeoJSON] = useState('');
-    const [file, setFile] = useState(null);
+    const [layerName, setLayerName] = useState<string>('');
+    const [linkToGeoJSON, setLinkToGeoJSON] = useState<string>('');
+    const [file, setFile] = useState<File | null>(null);
+    const [layerNameError, setLayerNameError] = useState<string>('');
+    const [linkOrFileError, setLinkOrFileError] = useState<string>('');
+    const [showLinkField, setShowLinkField] = useState<boolean>(true);
+
     const { showAddLayerWindow, openAddLayerWindow } = useToolContext();
 
-    const handleSubmit = () => {
-        console.log('Layer Name:', layerName);
-        console.log('Link or File:', linkToGeoJSON);
-        console.log('File:', file);
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        setLayerNameError('');
+        setLinkOrFileError('');
+
+        if (!layerName.trim()) {
+            setLayerNameError('Layer name is required');
+            return;
+        }
+
+        if (!(linkToGeoJSON || file)) {
+            setLinkOrFileError('Link or File is required');
+            return;
+        }
+
+        if (layerName && (linkToGeoJSON || file)) {
+            console.log('layerName: ', layerName);
+            console.log('linkToGeoJSON: ', linkToGeoJSON);
+            console.log('file: ', file);
+
+            setLayerName('');
+            setLinkToGeoJSON('');
+            setFile(null);
+        }
     };
 
     return (
         <Modal
             show={showAddLayerWindow}
-            onHide={() => openAddLayerWindow(false)}
-            dialogClassName="add-layer-window">
+            onHide={() => openAddLayerWindow(false)}>
             <Modal.Header closeButton>
                 <Modal.Title>Add layer</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                <Form noValidate onSubmit={event => event.preventDefault()}>
+            <Form noValidate onSubmit={handleSubmit}>
+                <Modal.Body>
                     <Row>
                         <Form.Group controlId="layerNameId">
-                            <Form.Label>Layer Name</Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder="Enter layer name"
@@ -37,46 +61,82 @@ const AddLayer: React.FC = () => {
                                 }
                                 required
                             />
+                            <Form.Text className="text-danger">
+                                {layerNameError}
+                            </Form.Text>
                         </Form.Group>
                     </Row>
                     <br />
                     <Row>
-                        <Form.Group controlId="linkToGeoJSONId">
-                            <Form.Label>Link</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Enter link or choose file"
-                                value={linkToGeoJSON}
-                                onChange={event =>
-                                    setLinkToGeoJSON(event.target.value)
-                                }
-                            />
+                        <Form.Group controlId="submitTypeId">
+                            <div className="add-layer-field">
+                                <Form.Check
+                                    type="radio"
+                                    name="submitType"
+                                    value="linkField"
+                                    checked={showLinkField}
+                                    onChange={event => {
+                                        setLinkToGeoJSON('');
+                                        setFile(null);
+                                        setShowLinkField(
+                                            event.target.value === 'linkField'
+                                        );
+                                    }}
+                                />
+                                <Form.Group controlId="linkToGeoJSONId">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter link"
+                                        value={linkToGeoJSON}
+                                        onChange={event =>
+                                            setLinkToGeoJSON(event.target.value)
+                                        }
+                                        disabled={!showLinkField}
+                                    />
+                                </Form.Group>
+                            </div>
+                            <br />
+                            <div className="add-layer-field">
+                                <Form.Check
+                                    type="radio"
+                                    name="submitType"
+                                    value="fileField"
+                                    checked={!showLinkField}
+                                    onChange={event => {
+                                        setLinkToGeoJSON('');
+                                        setFile(null);
+                                        setShowLinkField(
+                                            event.target.value !== 'fileField'
+                                        );
+                                    }}
+                                />
+                                <Form.Group controlId="fileId">
+                                    <Form.Control
+                                        type="file"
+                                        onChange={(event: any) =>
+                                            setFile(event.target.files[0])
+                                        }
+                                        disabled={showLinkField}
+                                    />
+                                    <Form.Text className="text-danger">
+                                        {linkOrFileError}
+                                    </Form.Text>
+                                </Form.Group>
+                            </div>
                         </Form.Group>
                     </Row>
-                    <br />
-                    <Row>
-                        <Form.Group controlId="fileId">
-                            <Form.Label>File</Form.Label>
-                            <Form.Control
-                                type="file"
-                                onChange={(event: any) =>
-                                    setFile(event.target.files[0])
-                                }
-                            />
-                        </Form.Group>
-                    </Row>
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button
-                    variant="secondary"
-                    onClick={() => openAddLayerWindow(false)}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={() => handleSubmit()}>
-                    Add layer
-                </Button>
-            </Modal.Footer>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="secondary"
+                        onClick={() => openAddLayerWindow(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" type="submit">
+                        Add layer
+                    </Button>
+                </Modal.Footer>
+            </Form>
         </Modal>
     );
 };
